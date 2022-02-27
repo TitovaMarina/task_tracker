70fce47149e9a71fc95059c44c4974ca6d6968fe
Марина Титова <mapuho4ka_98@mail.ru>
2022-02-27 19:02:10 +0300

    added dependency reset-css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 function App() {
   return (
     <div>
-      <header>
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-      </header>
+      test
     </div>
   );
 }
